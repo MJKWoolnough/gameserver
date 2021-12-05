439b6c7bb556f0f2c788b1fe95a0c74496e17d6d
Michael Woolnough <michael.woolnough@gmail.com>
2021-12-05 12:05:44 +0000

    added more socket infrastructure

--- socket.go
+++ socket.go
@@ -1,9 +1,13 @@
 package gameserver
 
 import (
+	"encoding/json"
+	"errors"
+	"strings"
 	"sync"
 
 	"golang.org/x/net/websocket"
+	"vimagination.zapto.org/jsonrpc"
 )
 
 type socket struct {
@@ -19,8 +23,42 @@ func newSocket() *socket {
 }
 
 func (s *socket) ServeConn(wconn *websocket.Conn) {
+	c := new(conn)
+	s.mu.Lock()
+	s.nextID++
+	id := s.nextID
+	s.conns[c] = struct{}{}
+	s.mu.Unlock()
+	c.ID = id
+	c.rpc = jsonrpc.New(wconn, c)
+	c.rpc.Handle()
+	s.mu.Lock()
+	delete(s.conns, c)
+	s.mu.Unlock()
 }
 
 type conn struct {
-	ID uint64
+	rpc *jsonrpc.Server
+	ID  uint64
 }
+
+func (c *conn) HandleRPC(method string, data json.RawMessage) (interface{}, error) {
+	switch method {
+	default:
+		pos := strings.IndexByte(method, '.')
+		if pos < 0 {
+			return nil, ErrUnkownEndpoint
+		}
+		submethod := method[pos+1:]
+		method = method[:pos]
+		switch method {
+		}
+		_ = submethod
+	}
+	return nil, ErrUnkownEndpoint
+}
+
+// Errors
+var (
+	ErrUnkownEndpoint = errors.New("unknown endpoint")
+)
