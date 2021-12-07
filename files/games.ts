94aafce325e5802c0e20bdbbdb243615ac08982b
Michael Woolnough <michael.woolnough@gmail.com>
2021-12-07 16:41:22 +0000

    show (sorted) room list

--- files/games.ts
+++ files/games.ts
@@ -1,4 +1,14 @@
-import {ready} from './room.js';
+import {clearElement} from './lib/dom.js';
+import {createHTML, h1, li} from './lib/html.js';
+import {node, stringSort} from './lib/nodes.js';
+import {room, ready} from './room.js';
 
 ready.then(() => {
+	const rooms = room.rooms();
+	rooms.sort((a, b) => a.room === "default" ? -1 : b.room === "default" ? 1 : stringSort(a.room, b.room));
+	room.roomFormatter((room: string) => li({"onclick": () => alert(room)}, room));
+	createHTML(clearElement(document.body), [
+		h1("Game Server"),
+		createHTML(rooms[node], {"id": "roomList"})
+	]);
 });
