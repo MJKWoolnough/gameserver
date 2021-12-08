6a1bf3cd082f12dee9779f5f59ae5987027a040c
Michael Woolnough <michael.woolnough@gmail.com>
2021-12-08 10:57:44 +0000

    added username entry box + addRoom button

--- files/games.ts
+++ files/games.ts
@@ -1,14 +1,21 @@
 import {clearElement} from './lib/dom.js';
-import {createHTML, h1, li} from './lib/html.js';
+import {createHTML, button, div, h1, input, label, li, span} from './lib/html.js';
 import {node, stringSort} from './lib/nodes.js';
 import {room, ready} from './room.js';
 
 ready.then(() => {
-	const rooms = room.rooms();
+	const rooms = room.rooms(),
+	      username = input({"type": "text", "id": "username", "placeholder": "Enter Username Here", "value": window.localStorage.getItem("username") ?? "", "onchange": () => window.localStorage.setItem("username", username.value)});
 	rooms.sort((a, b) => a.room === "default" ? -1 : b.room === "default" ? 1 : stringSort(a.room, b.room));
-	room.roomFormatter((room: string) => li({"onclick": () => alert(room)}, room));
+	room.roomFormatter((room: string) => li(span({"onclick": () => alert(room)}, room)));
 	createHTML(clearElement(document.body), [
 		h1("Game Server"),
-		createHTML(rooms[node], {"id": "roomList"})
+		label({"for": "username"}, "Username: "),
+		username,
+		div([
+			createHTML(rooms[node], {"id": "roomList"}),
+			button({"onclick": () => {
+			}}, "New Room")
+		]),
 	]);
 });
