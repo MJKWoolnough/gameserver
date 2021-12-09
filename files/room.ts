74470e3575798f64b906f8096eea2cfd6d4fadec
Michael Woolnough <michael.woolnough@gmail.com>
2021-12-09 11:45:55 +0000

    use becomeAdmin div to allow users to become admin of admin-less room

--- files/room.ts
+++ files/room.ts
@@ -1,7 +1,8 @@
 import {Requester} from './lib/inter.js';
-import {li, ul} from './lib/html.js';
+import {createHTML, li, ul} from './lib/html.js';
 import {node, NodeArray} from './lib/nodes.js';
 import RPC from './lib/rpc_ws.js';
+import {becomeAdmin} from './script.js';
 
 const broadcastRoomAdd = -1, broadcastRoomRemove = -2, broadcastAdminNone = -3, broadcastAdmin = -4, broadcastUserJoin = -5, broadcastUserLeave = -6, broadcastMessage = -7;
 
@@ -40,7 +41,6 @@ export const room = {} as {
 	setStatus: (data: GameMessage) => Promise<void>;
 	message: (msg: any) => Promise<void>;
 	messageHandler: (fn: (data: any) => void) => void;
-	adminChange: (fn: (data: string) => void) => void;
 	username: () => string;
 	userFormatter: (fn: (username: string) => HTMLLIElement) => void;
 	roomFormatter: (fn: (room: string) => HTMLLIElement) => void;
@@ -78,15 +78,23 @@ ready = pageLoad.then(() => RPC(`ws${protocol.slice(4)}//${host}/socket`, 1.1)).
 				for (const user of u) {
 					users.push({user, [node]: userFormatter(user)});
 				}
+				if (!admin) {
+					setTimeout(() => createHTML(document.body, becomeAdmin), 0);
+				}
 				return status;
 			});
 		},
-		"leave": () => rpc.request("leaveRoom"),
-		"makeAdmin": () => rpc.request("adminRoom").then(() => admin = username),
+		"leave": () => {
+			createHTML(document.body, becomeAdmin);
+			return rpc.request("leaveRoom");
+		},
+		"makeAdmin": () => rpc.request("adminRoom").then(() => {
+			becomeAdmin.remove();
+			admin = username;
+		}),
 		"setStatus": (status: GameMessage) => rpc.request("setStatus", status),
 		"message": (message: any) => rpc.request("message", message),
 		"messageHandler": messages.responder.bind(messages),
-		"adminChange": adminChange.responder.bind(adminChange),
 		"username": () => username,
 		"userFormatter": (fn: (username: string) => HTMLLIElement) => {
 			userFormatter = fn;
@@ -108,8 +116,14 @@ ready = pageLoad.then(() => RPC(`ws${protocol.slice(4)}//${host}/socket`, 1.1)).
 	});
 	rpc.await(broadcastRoomAdd, true).then(room => rooms.push({room, [node]: roomFormatter(room)}));
 	rpc.await(broadcastRoomRemove, true).then(room => rooms.filterRemove(r => r.room === room));
-	rpc.await(broadcastAdminNone, true).then(() => adminChange.request(admin = ""));
-	rpc.await(broadcastAdmin, true).then((a: string) => adminChange.request(admin = a));
+	rpc.await(broadcastAdminNone, true).then(() => {
+		admin = "";
+		createHTML(document.body, becomeAdmin);
+	});
+	rpc.await(broadcastAdmin, true).then((a: string) => {
+		becomeAdmin.remove();
+		adminChange.request(admin = a);
+	});
 	rpc.await(broadcastUserJoin, true).then((user: string) => users.push({user, [node]: userFormatter(user)}));
 	rpc.await(broadcastUserLeave, true).then(user => {
 		const pos = rooms.indexOf(user);
