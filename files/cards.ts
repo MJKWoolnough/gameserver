82858303ecf3fe21f61e60dbf792996930e7680d
Michael Woolnough <michael.woolnough@gmail.com>
2021-12-10 11:45:14 +0000

    added diamond symbol

--- files/cards.ts
+++ files/cards.ts
@@ -1,8 +1,9 @@
 import type {Props} from './lib/dom.js';
-import {defs, svg, use} from './lib/svg.js';
+import {defs, path, svg, use} from './lib/svg.js';
 
 export const cards = svg({"style": "width: 0; height: 0"}, [
 	defs([
+		path({"id": "diamond", "d": "M37,0 l32,42 -32,42 -32,-42 z", "fill": "#f00"}),
 	])
 ]),
 cardSymbol = (props: Props = {}, id: number) => svg(props, use({"href": "#card_" + id})),
