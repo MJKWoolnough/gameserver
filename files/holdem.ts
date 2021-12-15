0f74e86bf6190a4b2db4bb5f84848cd69aa1a12a
Michael Woolnough <michael.woolnough@gmail.com>
2021-12-15 13:41:46 +0000

    added holdem options

--- files/holdem.ts
+++ files/holdem.ts
@@ -1,7 +1,30 @@
+import {clearElement} from './lib/dom.js';
+import {createHTML, br, button, div, input, label} from './lib/html.js';
 import games from './games.js';
 
 const name = "Texas Hold'Em";
 
-games.set(name, (admin: boolean, status?: any) => {
-
+games.set(name, (admin: boolean, _status?: any) => {
+	if (admin) {
+		const minimumBet = input({"id": "minimum", "type": "number", "min": 0, "value": 2}),
+		      ante = input({"id": "ante", "type": "number", "min": 0, "value": 0}),
+		      limit = input({"id": "limit", "name": "limit", "type": "radio", "checked": true}),
+		      potLimit = input({"id": "potLimit", "name": "limit", "type": "radio"}),
+		      noLimit = input({"id": "noLimit", "name": "limit", "type": "radio"});
+		createHTML(clearElement(document.body), div({"id": "holdem"}, [
+			label({"for": "minimum"}, "Minimum Bet: "),
+			minimumBet,
+			br(),
+			label({"for": "ante"}, "Ante: "),
+			ante,
+			br(),
+			limit,
+			label({"for": "limit"}, "Limit"),
+			potLimit,
+			label({"for": "potLimit"}, "Pot Limit"),
+			noLimit,
+			label({"for": "noLimit"}, "No Limit"),
+			button({"onclick": () => {}}, "Done")
+		]));
+	}
 });
