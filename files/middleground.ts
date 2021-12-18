9761bf8aee63f3c57f1d4f6eb5b069d63a2dc756
Michael Woolnough <michael.woolnough@gmail.com>
2021-12-18 16:07:08 +0000

    rewrote to work with new Room layout

--- files/middleground.ts
+++ files/middleground.ts
@@ -3,39 +3,32 @@ import {createHTML, div, h1, h2, input, label, li, ul} from './lib/html.js';
 import games from './games.js';
 import {room} from './room.js';
 
-type mData = {
+type Data = {
 	players?: [string, string];
 	words: [string, string][];
 }
 
 const game = "Middleground",
-      word = input({"type": "text", "value": ""}),
-      messageHandler = (data: mData) => createHTML(clearElement(document.body), {"id": "mg"}, [h1(game), !data.players ? h2("Waiting for game to begin...") : [
-	div(data.players[0]),
-	div(data.players[1]),
-	ul(data.words.map(([a, b]) => li([div(a), div(b)]))),
-	data.players.includes(room.username()) ? [
-		createHTML(word, {"value": ""}),
-		input({"id": "confirm", "type": "checkbox", "onchange": function (this: HTMLInputElement) {
-			if (room.admin() === room.username()) {
+      word = input({"type": "text", "value": ""});
 
-			} else {
-				room.message({"word": this.checked ? word.value : ""});
-			}
-		}}),
-		label({"for": "confirm"})
-	] : []
-      ]]),
-      runAdmin = () => {};
+games.set(game, {
+	"onAdmin": () => {},
+	"onRoomMessage": (data: Data) => {
+		createHTML(clearElement(document.body), {"id": "mg"}, [h1(game), !data.players ? h2("Waiting for game to begin...") : [
+			div(data.players[0]),
+			div(data.players[1]),
+			ul(data.words.map(([a, b]) => li([div(a), div(b)]))),
+			data.players.includes(room.username()) ? [
+				createHTML(word, {"value": ""}),
+				input({"id": "confirm", "type": "checkbox", "onchange": function (this: HTMLInputElement) {
+					if (room.admin() === room.username()) {
 
-games.set(game, (admin: boolean, status?: any) => {
-	if (admin) {
-		runAdmin();
-	} else {
-		room.messageHandler(messageHandler);
-		room.onAdmin(runAdmin);
-	}
-	if (status) {
-		messageHandler(status);
+					} else {
+						room.messageAdmin({"word": this.checked ? word.value : ""});
+					}
+				}}),
+				label({"for": "confirm"})
+			] : []
+		]]);
 	}
 });
