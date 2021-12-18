3c843a9caaa9d5e020dc81e5aa938e30f7edafef
Michael Woolnough <michael.woolnough@gmail.com>
2021-12-18 10:32:20 +0000

    WIT now can transfer control to a new admin

--- files/wit.ts
+++ files/wit.ts
@@ -5,7 +5,46 @@ import {room} from './room.js';
 import {files} from './wit_data.js';
 
 const game = "What is That?",
-      boxes = [10, 12, 16, 22, 30, 40, 52, 66, 82];
+      boxes = [10, 12, 16, 22, 30, 40, 52, 66, 82],
+      runAdmin = () => {
+	let image = 0, step = 0;
+	const shuffledFiles = Array.from({"length": files.length}, () => files.splice(Math.floor(Math.random() * files.length), 1)[0]),
+	      l = shuffledFiles.length,
+	      sendStatus = () => {
+		      image = ((image % l) + l) % l;
+		      step = Math.max(Math.min(step, boxes.length), 0);
+		      const data: any = {game, "url": shuffledFiles[image][1], step};
+		      if (step === boxes.length) {
+			      data["title"] = shuffledFiles[image][0];
+		      }
+		      createHTML(progress, `Image: ${image + 1}/${l} - Step ${step + 1}/${boxes.length + 1}`);
+		      room.message(data);
+	      },
+	      progress = div({"style": {"text-align": "center", "font-size": "2em"}});
+	sendStatus();
+	createHTML(clearElement(document.body), [
+		h1({"style": "text-align: center"}, game),
+		progress,
+		button({"class": "witButton", "onclick": () => {
+			image--;
+			step = 0;
+			sendStatus();
+		}}, "Previous Image"),
+		button({"class": "witButton big", "onclick": () => {
+			step--;
+			sendStatus();
+		}}, "Previous Step"),
+		button({"class": "witButton big", "onclick": () => {
+			step++;
+			sendStatus();
+		}}, "Next Step"),
+		button({"class": "witButton", "onclick": () => {
+			image++;
+			step = 0;
+			sendStatus();
+		}}, "Next Image")
+	]);
+      };
 
 type Message = {
 	url: string;
@@ -15,44 +54,9 @@ type Message = {
 
 games.set(game, (admin: boolean, status?: any) => {
 	if (admin) {
-		let image = 0, step = 0;
-		const shuffledFiles = Array.from({"length": files.length}, () => files.splice(Math.floor(Math.random() * files.length), 1)[0]),
-		      l = shuffledFiles.length,
-		      sendStatus = () => {
-			      image = ((image % l) + l) % l;
-			      step = Math.max(Math.min(step, boxes.length), 0);
-			      const data: any = {game, "url": shuffledFiles[image][1], step};
-			      if (step === boxes.length) {
-				      data["title"] = shuffledFiles[image][0];
-			      }
-			      createHTML(progress, `Image: ${image + 1}/${l} - Step ${step + 1}/${boxes.length + 1}`);
-			      room.message(data);
-		      },
-		      progress = div({"style": {"text-align": "center", "font-size": "2em"}});
-		sendStatus();
-		createHTML(clearElement(document.body), [
-			h1({"style": "text-align: center"}, game),
-			progress,
-			button({"class": "witButton", "onclick": () => {
-				image--;
-				step = 0;
-				sendStatus();
-			}}, "Previous Image"),
-			button({"class": "witButton big", "onclick": () => {
-				step--;
-				sendStatus();
-			}}, "Previous Step"),
-			button({"class": "witButton big", "onclick": () => {
-				step++;
-				sendStatus();
-			}}, "Next Step"),
-			button({"class": "witButton", "onclick": () => {
-				image++;
-				step = 0;
-				sendStatus();
-			}}, "Next Image")
-		]);
+		runAdmin();
 	} else {
+		room.onAdmin = runAdmin;
 		const title = div({"style": {"position": "absolute", "bottom": 0, "left": 0, "right": 0, "text-align": "center", "color": "#fff", "text-shadow": "-1px -1px 0 #000, 1px -1px 0 #000, -1px 1px 0 #000, 1px 1px 0 #000", "font-size": "5em"}}),
 		      mh = (message: Message) => {
 			const c = canvas({"style": {"image-rendering": "pixelated", "max-width": "100%", "max-height": "100%", "width": "100%", "object-fit": "contain"}}),
