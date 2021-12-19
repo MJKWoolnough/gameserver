7a8cd5b099bfd9779abfbd4b55478d204461e122
Michael Woolnough <michael.woolnough@gmail.com>
2021-12-19 12:45:17 +0000

    added method to get questions

--- files/otdb.ts
+++ files/otdb.ts
@@ -17,6 +17,28 @@ type CategoryResponse = {
 	trivia_categories: Category[];
 }
 
+type QuestionFilter = {
+	amount: number;
+	category?: number;
+	difficulty?: "easy" | "medium" | "hard";
+	type?: "multiple" | "boolean";
+	autoReset?: boolean;
+}
+
+type QuestionResponse = {
+	response_code: number;
+	results: Question[];
+}
+
+type Question = {
+	category: string;
+	type: "multiple" | "boolean";
+	difficulty: "easy" | "medium" | "hard";
+	question: string;
+	correct_answer: string;
+	incorrect_answers: string[];
+}
+
 class OTDB {
 	#sessionID: string;
 	categories: Map<string, number>;
@@ -26,6 +48,26 @@ class OTDB {
 		for (const {id, name} of cats) {
 			this.categories.set(name, id);
 		}
+	}
+	getQuestions(filter: QuestionFilter = {"amount": 1}): Promise<Question[]> {
+		return (HTTPRequest(`https://opentdb.com/api.php?amount=${Math.min(Math.max(filter.amount, 1), 50)}${filter.category ? `&category=${filter.category}` : ""}${filter.difficulty ? `&difficulty=${filter.difficulty}` : ""}${filter.type ? `&type=${filter.type}` : ""}`, params) as Promise<QuestionResponse>).then(qr => {
+			switch (qr.response_code) {
+			case 0:
+				return qr.results;
+			case 1:
+				throw new Error("no results");
+			case 2:
+				throw new Error("invalid param");
+			case 3:
+			case 4:
+				if (filter.autoReset) {
+					return this.resetToken().then(() => this.getQuestions(filter));
+				} else {
+					throw new Error("no more results");
+				}
+			}
+			throw new Error("unknown error");
+		});
 	}
 	resetToken() {
 		return (HTTPRequest(`https://opentdb.com/api_token.php?command=reset&token=${this.#sessionID}`, params) as Promise<TokenResponse>).then(token => {
