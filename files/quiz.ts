312057e9c836f5543e00b66212a1fbe32d98c263
Michael Woolnough <michael.woolnough@gmail.com>
2021-12-21 13:44:18 +0000

    put quiz options in fn to be repeatedly callable

--- files/quiz.ts
+++ files/quiz.ts
@@ -1,5 +1,5 @@
 import {clearElement} from './lib/dom.js';
-import {createHTML, br, button, h1, h2, input, label, li, ul} from './lib/html.js';
+import {createHTML, br, button, div, h1, h2, input, label, li, ul} from './lib/html.js';
 import games from './games.js';
 import otdb from './otdb.js';
 
@@ -9,35 +9,38 @@ games.set(game, {
 	"onAdmin": () => {
 		createHTML(clearElement(document.body), h1("Creating OpenTrivia Database Connection"));
 		otdb().then(o => {
-			let round = 1;
-			const timer = input({"type": "number", "min": 0, "value": 0}),
-			      showAnswers = input({"type": "checkbox", "checked": true}),
-			      cats = new Set<number>(),
-			      numberQs = input({"type": "number", "min": 1, "max": 50, "value": 10});
-			createHTML(clearElement(document.body), [
-				h1(`Round ${round}`),
-				label({"for": "timer"}, "Timer (s): "),
-				timer,
-				br(),
-				label({"for": "showAnswers"}, "Show Answers: "),
-				showAnswers,
-				br(),
-				label({"for": "numberQs"}, "Number of Questions: "),
-				numberQs,
-				br(),
-				h2("Categories"),
-				ul(Array.from(o.categories.entries()).map(([cat, id]) => li([
-					input({"id": `cat_${id}`, "type": "checkbox", "onclick": function(this: HTMLInputElement) {
-						if (this.checked) {
-							cats.add(id);
-						} else {
-							cats.delete(id);
-						}
-					}}),
-					label({"for": `cat_${id}`}, cat)
-				]))),
-				button({"onclick": () => {}}, "Start")
-			]);
+			let round = 0;
+			const roundStart = () => {
+				const timer = input({"type": "number", "min": 0, "value": 0}),
+				      showAnswers = input({"type": "checkbox", "checked": true}),
+				      cats = new Set<number>(),
+				      numberQs = input({"type": "number", "min": 1, "max": 50, "value": 10});
+				createHTML(clearElement(document.body), div({"id": "quizOptions"}, [
+					h1(`Round ${++round}`),
+					label({"for": "timer"}, "Timer (s): "),
+					timer,
+					br(),
+					label({"for": "numberQs"}, "Number of Questions: "),
+					numberQs,
+					br(),
+					label({"for": "showAnswers"}, "Show Answers: "),
+					showAnswers,
+					br(),
+					h2("Categories"),
+					ul(Array.from(o.categories.entries()).map(([cat, id]) => li([
+						input({"id": `cat_${id}`, "type": "checkbox", "onclick": function(this: HTMLInputElement) {
+							if (this.checked) {
+								cats.add(id);
+							} else {
+								cats.delete(id);
+							}
+						}}),
+						label({"for": `cat_${id}`}, cat)
+					]))),
+					button({"onclick": () => {}}, "Start")
+				]));
+			      };
+			roundStart();
 		}).catch(alert);
 	},
 	"onRoomMessage": () => {}
