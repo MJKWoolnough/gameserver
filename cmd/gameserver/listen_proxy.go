03f47f65aca957e96ed710ed78f59f9c331ff701
Michael Woolnough <michael.woolnough@gmail.com>
2021-12-22 13:05:03 +0000

    added user specifiable Proxy port

--- cmd/gameserver/listen_proxy.go
+++ cmd/gameserver/listen_proxy.go
@@ -3,11 +3,18 @@
 package main
 
 import (
+	"flag"
+	"fmt"
 	"net"
 
 	"vimagination.zapto.org/reverseproxy/unixconn"
 )
 
+var p = flag.Uint("p", 80, "Proxy Listen Port")
+
 func listen() (net.Listener, error) {
-	return unixconn.Listen("tcp", ":80")
+	if p > 65535 || p == 0 {
+		p = 80
+	}
+	return unixconn.Listen("tcp", fmt.Sprintf(":%d", p))
 }
