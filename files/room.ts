6fc8f569186f7bdec3fc42065bd0259d69d83d8b
Michael Woolnough <michael.woolnough@gmail.com>
2022-01-11 09:06:33 +0000

    moved room selection code to room module

--- files/room.ts
+++ files/room.ts
@@ -1,6 +1,6 @@
 import {clearElement, makeElement} from './lib/dom.js';
-import {div, h1, li, ul} from './lib/html.js';
-import {node, NodeArray} from './lib/nodes.js';
+import {button, div, h1, input, li, span, ul} from './lib/html.js';
+import {node, NodeArray, stringSort} from './lib/nodes.js';
 import RPC from './lib/rpc_ws.js';
 
 type RoomNode = {
@@ -153,4 +153,46 @@ ready = pageLoad.then(() => RPC(`ws${protocol.slice(4)}//${host}/socket`, 1.1)).
 			rooms.push({room, [node]: roomFormatter(room)});
 		}
 	}));
+});
+
+ready.then(() => {
+	if (new URLSearchParams(window.location.search).has("monitor")) {
+		room.join("default", "");
+		return;
+	}
+	const rooms = room.rooms(),
+	      username = input({"type": "text", "id": "username", "maxlength": 100, "placeholder": "Spectate or Enter Username Here", "value": window.localStorage.getItem("username") ?? "", "onchange": () => window.localStorage.setItem("username", username.value)}),
+	      error = span({"id": "error"});
+	rooms.sort((a, b) => a.room === "default" ? -1 : b.room === "default" ? 1 : stringSort(a.room, b.room));
+	room.roomFormatter((r: string) => li(button({"onclick": () => room.join(r, username.value).catch((e: Error) => makeElement(error, e.message))}, r)));
+	makeElement(clearElement(document.body), [
+		h1("Game Server"),
+		username,
+		error,
+		makeElement(rooms[node], {"id": "roomList"}),
+		button({"onclick": () => {
+			const roomName = prompt("Please enter new Room name");
+			if (roomName && roomName.length <= 100) {
+				room.new(roomName, username.value).catch((e: Error) => alert(e.message));
+			}
+		}}, "New Room")
+	]);
+});
+
+games.set("", {
+	"onAdmin": () => {
+		const gameList = new NodeArray<{game: string, [node]: HTMLLIElement}>(ul({"id": "gameList"}), (a, b) => stringSort(a.game, b.game));
+		for (const game of games.keys()) {
+			if (game) {
+				gameList.push({game, [node]: li(button({"onclick": () => room.adminGame(game)}, game))});
+			}
+		}
+		makeElement(clearElement(document.body), [
+			h1("Choose Game"),
+			gameList[node]
+		]);
+	},
+	"onRoomMessage": () => {
+		makeElement(clearElement(document.body), h1("Waiting for Game"));
+	}
 });
