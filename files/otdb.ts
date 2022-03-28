3f672aa23d8dcbf86fcc2494f6777c1602d84073
Michael Woolnough <michael.woolnough@gmail.com>
2022-03-28 14:08:36 +0100

    removed remote API for OTDB for ease of use

--- files/otdb.ts
+++ files/otdb.ts
@@ -1,32 +1,3 @@
-import {HTTPRequest} from './lib/conn.js';
-
-type TokenResponse = {
-	response_code: number;
-	response_message: string;
-	token: string;
-}
-
-type Category = {
-	id: number;
-	name: string;
-}
-
-type CategoryResponse = {
-	trivia_categories: Category[];
-}
-
-type CategoryCount = {
-	total_num_of_questions: number;
-	total_num_of_pending_questions: number;
-	total_num_of_verified_questions: number;
-	total_num_of_rejected_questions: number;
-}
-
-type CategoryCountResponse = {
-	overall: CategoryCount;
-	categories: Record<string, CategoryCount>;
-}
-
 type Difficulty = "easy" | "medium" | "hard";
 
 type Type = "multiple" | "boolean";
@@ -37,11 +8,6 @@ export type QuestionFilter = {
 	difficulty?: Difficulty;
 	type?: Type;
 	autoReset?: boolean;
-}
-
-type QuestionResponse = {
-	response_code: number;
-	results: Question[];
 }
 
 export type Question = {
@@ -61,68 +27,12 @@ export interface OTDB {
 
 type QuestionData = [0, 0 | 1 | 2, number, string, 0 | 1] | [1, 0 | 1 | 2, number, string, string, ...string[]];
 
-let categories: [string, number][] | null = null,
-    imported: Promise<QuestionData[]> | null = null;
+let imported: Promise<QuestionData[]> | null = null;
 
-const counts: [number, number][] = [],
-      params = {"response": "json"} as const,
-      fields = ["category", "type", "difficulty", "question", "correct_answer"],
-      errors = ["", "No Results", "Invalid Parameter", "Token Not Found", "Token Empty"],
-      reject = (reason: string) => Promise.reject(reason),
-      types = ["boolean", "multiple"] as Type[],
+const types = ["boolean", "multiple"] as Type[],
       difficulties = ["easy", "medium", "hard"] as Difficulty[],
       booleans = ["False", "True"],
       iCats: string[] = [];
-
-class otdbNet {
-	#sessionID: string;
-	categories: ReadonlyMap<string, number>;
-	#counts: Map<number, number>;
-	constructor (sessionID: string, cats: Map<string, number>, counts: Map<number, number>) {
-		this.#sessionID = sessionID;
-		this.categories = cats;
-		this.#counts = counts;
-	}
-	getQuestions(filter: QuestionFilter = {"amount": 1}): Promise<Question[]> {
-		filter.category = filter.category === undefined ? -1 : this.#counts.has(filter.category) ? filter.category : -1;
-		const amount = Math.min(Math.max(filter.amount, 1), 50, this.#counts.get(filter.category) || 0);
-		return amount === 0 ? filter.autoReset ? this.reset().then(() => this.getQuestions(filter)) : Promise.resolve([]) : HTTPRequest<QuestionResponse>(`https://opentdb.com/api.php?amount=${amount}${filter.category !== -1 ? `&category=${filter.category}` : ""}${filter.difficulty ? `&difficulty=${filter.difficulty}` : ""}${filter.type ? `&type=${filter.type}` : ""}&encode=base64`, params).then(({response_code, results}) => {
-			switch (response_code) {
-			case 0:
-				this.#counts.set(-1, this.#counts.get(-1)! - results.length);
-				for (const question of results) {
-					for (const field of fields) {
-						(question as any)[field] = atob((question as any)[field]);
-					}
-					question.incorrect_answers = question.incorrect_answers.map(atob);
-					const catID = this.categories.get(question.category) || -2,
-					      num = this.#counts.get(catID);
-					if (num) {
-						this.#counts.set(catID, num - 1);
-					}
-				}
-				return results;
-			case 3:
-			case 4:
-				if (filter.autoReset) {
-					delete filter["autoReset"];
-					return this.reset().then(() => this.getQuestions(filter));
-				}
-			}
-			return reject(errors[response_code] || "Unknown Error");
-		});
-	}
-	reset() {
-		return HTTPRequest<TokenResponse>(`https://opentdb.com/api_token.php?command=reset&token=${this.#sessionID}`, params).then(token => {
-			if (token.response_code !== 0) {
-				return reject("could not retrieve token");
-			}
-			this.#sessionID = token.token;
-			this.#counts = new Map(counts);
-			return;
-		});
-	}
-}
 
 class otdbLocal {
 	#questions: Set<QuestionData>;
@@ -181,15 +91,4 @@ export default (): Promise<OTDB> => (imported ?? (imported = import("data/otdb.j
 	}
 	return qs;
 })))
-.then(qs => new otdbLocal(qs.concat()))
-.catch(() => (categories ? Promise.resolve() : Promise.all([
-	HTTPRequest<CategoryResponse>("https://opentdb.com/api_category.php", params).then(cats => categories = cats.trivia_categories.map(c => [c.name, c.id])),
-	HTTPRequest<CategoryCountResponse>("https://opentdb.com/api_count_global.php", params).then(catCounts => {
-		counts.push([-1, catCounts.overall.total_num_of_verified_questions]);
-		for (const [id, {total_num_of_verified_questions}] of Object.entries(catCounts.categories)) {
-			counts.push([parseInt(id), total_num_of_verified_questions]);
-		}
-	})
-]))
-.then(() => HTTPRequest<TokenResponse>("https://opentdb.com/api_token.php?command=request", params))
-.then(token => token.response_code ? reject(token.response_message) : new otdbNet(token.token, new Map(categories), new Map(counts))));
+.then(qs => new otdbLocal(qs.concat()));
