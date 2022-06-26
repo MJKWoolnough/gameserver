255b64e0874a9fa6a1f9ac6867bd03c2af761342
Michael Woolnough <michael.woolnough@gmail.com>
2022-06-26 11:05:13 +0100

    removed unneeded comma

--- files/cards.ts
+++ files/cards.ts
@@ -64,7 +64,7 @@ cards = svg({"style": {"width": 0, "height": 0}}, [
 				use({"href": "#card"}),
 				["", "rotate(180, 125, 175) "].map(rotate => [
 					use({"href": `#num_${num}`, "transform": `${rotate}translate(2, 10) scale(0.5)`, stroke}),
-					use({"href": `#suit_${suit}`, "transform": `${rotate}translate(6, 55) scale(0.4)`}),
+					use({"href": `#suit_${suit}`, "transform": `${rotate}translate(6, 55) scale(0.4)`})
 				]),
 				num === 0 ? use({"href": `#suit_${suit}`, "transform": "translate(53, 91) scale(2)"}) : num < 10 ? symbolPlaces[num - 1].map(placement => use({"href": `#suit_${suit}`, "transform": `scale(0.7) translate(${(placement[0] + +(placement[1] > 175)) / 0.7}, ${placement[1] / 0.7})` + (placement[1] > 175 ? " rotate(180, 36, 42)" : "")})) : use({"href": `#num_${num}`, "transform": "translate(53, 91) scale(2)", stroke})
 			]);
