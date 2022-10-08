0abd75c5d3332e0511ea75cf542de9a2637d9e10
Michael Woolnough <michael.woolnough@gmail.com>
2022-10-08 07:23:53 +0100

    converted CSS to use CSS lib

--- files/middleground.ts
+++ files/middleground.ts
@@ -1,3 +1,4 @@
+import {add, ids, query} from './lib/css.js';
 import {amendNode, clearNode} from './lib/dom.js';
 import {button, div, h1, input, li, ul} from './lib/html.js';
 import {node} from './lib/nodes.js';
@@ -13,7 +14,7 @@ const game = "Middleground",
       users = new Set<string>(),
       showUI = (data: Data, fn: (word: string) => void) => {
 	const word = input({"type": "text", "placeholder": "Word Here"});
-	return clearNode(document.body, {"id": "mg"}, [
+	return clearNode(document.body, {"id": mgID}, [
 		h1(game),
 		div(data.players[0]),
 		div(data.players[1]),
@@ -37,7 +38,7 @@ const game = "Middleground",
 		      data = {players, words},
 		      selectUsers = () => {
 			words.splice(0, words.length);
-			clearNode(document.body, {"id": "mgSelect"}, [
+			clearNode(document.body, {"id": mgSelectID}, [
 				room.users()[node],
 				button({"onclick": () => {
 					if (users.size === 2) {
@@ -82,12 +83,114 @@ const game = "Middleground",
 	},
 	"userFormatter": (username: string) => li({"onclick": function(this: HTMLInputElement) {
 		const selected = !users.has(username);
-		amendNode(this, {"class": {selected}});
+		amendNode(this, {"class": {[selectedID]: selected}});
 		users[selected ? "add" : "delete"](username);
 	}}, username),
 	"onUserLeave": (username: string) => users.delete(username),
 	"onMessage": noop as (player: string, word: string) => void ,
 	"onRoomMessage": (data: Data) => showUI(data, room.messageAdmin)
-      };
+      },
+      [mgSelectID, mgID, selectedID] = ids(3);
+
+add(`#${mgSelectID}`, {
+	" ul": {
+		"display": "grid",
+		"grid-gap": "2px",
+		"grid-template-columns": "repeat(auto-fit, minmax(20em, 1fr))"
+	},
+	" li": {
+		"box-sizing": "border-box",
+		"display": "flex",
+		"align-items": "center",
+		"justify-content": "center",
+		"text-align": "center",
+		"width": "100%",
+		"height": "100%",
+		"cursor": "pointer",
+		"font-size": "3em",
+		"background-color": "#080",
+		"border": "1vmax outset #0a0",
+		":active:hover": {
+			"border-style": "inset"
+		},
+		".selected": {
+			"background-color": "#008",
+			"border-color": "#00a"
+		}
+	},
+	" button": {
+		"width": "100%",
+		"box-sizing": "border-box",
+		"font-size": "3em",
+		"text-align": "center",
+		"background-color": "#800",
+		"border-color": "#a00",
+		"border-width": "1vmax",
+		"color": "#fff"
+	}
+});
+add(`#${mgID}`, {
+	" h1": {
+		"font-size": "3em"
+	},
+	" div": {
+		"width": "50%",
+		"display": "inline-block",
+		"text-align": "center",
+		"font-size": "3em"
+	},
+	" input": {
+		"width": "calc(100% - 5em)",
+		"box-sizing": "border-box",
+		"font-size": "3em",
+		"text-align": "center",
+		"[type=checkbox]": {
+			"display": "none"
+		},
+		":checked + label:after": {
+			"content": `"Ready!"`,
+			"background-color": "#080",
+			"border-color": "#0a0"
+		}
+	},
+	" label": {
+		":after": {
+			"box-sizing": "border-box",
+			"content": `"Ready?"`,
+			"display": "inline-block",
+			"width": "5em",
+			"font-size": "23em",
+			"background-color": "#800",
+			"border": "1vmax outset #a00",
+			"text-align": "center"
+		},
+		":active:hover:after": {
+			"border-style": "inset"
+		}
+	},
+	" button": {
+		"background-color": "#008",
+		"border-color": "#00a",
+		"border-width": "1vmax",
+		"font-size": "3em",
+		"text-align": "center",
+		"color": "#fff",
+		"width": "50%",
+		"box-sizing": "border-box"
+	}
+});
+query("@media (orientation: portrait)", {
+	[`#${mgID}`]: {
+		" button": {
+			"width": "100%",
+		},
+		" input": {
+			"width": "100%"
+		},
+		" label:after": {
+			"width": "100%"
+		}
+	}
+});
 
 addGame(game, gameObj);
